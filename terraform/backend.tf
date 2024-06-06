cad01083b4982a2c3174fd7b2c29a9c3e0004f8c
anandraowpp <anand.rao@wpp.com>
2024-06-06 06:25:55 +0530

    test

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "anand-terraform-free"
-    token = "XoE5pHoC0VfquA.atlasv1.RvvIQcRgob7u85OoUybbpiytcuaE7GtUMaGjR8o69SeULmBPfnMg3xWyR3CS8spZxzE"
+    # token = "XoE5pHoC0VfquA.atlasv1.RvvIQcRgob7u85OoUybbpiytcuaE7GtUMaGjR8o69SeULmBPfnMg3xWyR3CS8spZxzE"
 
     workspaces {
       name = "myoneworkspace"
