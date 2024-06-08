18a9eb75b091008ab9f63b68b19ce8f0f8c2fb35
anandraowpp <anand.rao@wpp.com>
2024-06-08 06:09:01 +0530

    t

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "anand-terraform-free"
-    # token = "XoE5pHoC0VfquA.atlasv1.RvvIQcRgob7u85OoUybbpiytcuaE7GtUMaGjR8o69SeULmBPfnMg3xWyR3CS8spZxzE"
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "anand-terraform-free"
+#     # token = "XoE5pHoC0VfquA.atlasv1.RvvIQcRgob7u85OoUybbpiytcuaE7GtUMaGjR8o69SeULmBPfnMg3xWyR3CS8spZxzE"
 
-    workspaces {
-      name = "api-workspace"
-    }
-  }
-}
+#     workspaces {
+#       name = "api-workspace"
+#     }
+#   }
+# }
