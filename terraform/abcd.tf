62f15a112e507434e01e181f7e198ed99d7c02aa
anandctx <anandctx@outlook.com>
2024-06-23 06:45:56 +0530

    tfplan

--- terraform/abcd.tf
+++ terraform/abcd.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "workspacestest"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example2" {
+  name     = "workspacestest2"
+  location = "West Europe"
+}
